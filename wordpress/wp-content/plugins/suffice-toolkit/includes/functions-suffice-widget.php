16fb69419a3362d70c101c8ee891388977e130ea
ben <ben.guo@codecanteen.com>
2017-10-30 23:55:24 +0800

    add widget immigration policy

--- wordpress/wp-content/plugins/suffice-toolkit/includes/functions-suffice-widget.php
+++ wordpress/wp-content/plugins/suffice-toolkit/includes/functions-suffice-widget.php
@@ -23,6 +23,7 @@ include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-cta.php' );
 include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-logo.php' );
 include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-portfolio.php' );
 include_once(dirname(__FILE__) . '/widgets/class-suffice-widget-university.php');
+include_once(dirname(__FILE__) . '/widgets/class-suffice-widget-Immigration.php');
 include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-team.php' );
 include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-slider.php' );
 include_once( dirname( __FILE__ ) . '/widgets/class-suffice-widget-testimonial.php' );
@@ -43,6 +44,7 @@ function suffice_register_widgets() {
 	register_widget( 'ST_Widget_Logo' );
 	register_widget( 'ST_Widget_Portfolio' );
 	register_widget( 'ST_Widget_University' );
+	register_widget( 'ST_Widget_Immigration' );
 	register_widget( 'ST_Widget_Team' );
 	register_widget( 'ST_Widget_Slider' );
 	register_widget( 'ST_Widget_Testimonial' );
@@ -66,6 +68,7 @@ function suffice_toolkit_widgets($widgets) {
 		'ST_Widget_Logo',
 		'ST_Widget_Portfolio',
 		'ST_Widget_University',
+		'ST_Widget_Immigration',
 		'ST_Widget_Team',
 		'ST_Widget_Slider',
 		'ST_Widget_Testimonial',
---
+++ wordpress/wp-content/plugins/suffice-toolkit/templates/content-widget-immigration.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * The template for displaying portfolio widget.
+ *
+ * This template can be overridden by copying it to yourtheme/suffice-toolkit/content-widget-portfolio.php.
+ *
+ * HOWEVER, on occasion SufficeToolkit will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     http://docs.themegrill.com/suffice-toolkit/template-structure/
+ * @author  ThemeGrill
+ * @package SufficeToolkit/Templates
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$categories    = isset( $instance['categories'] ) ? $instance['categories'] : '';
+$number        = isset( $instance['number'] ) ? $instance['number'] : '';
+$filter        = empty( $instance['filter'] ) ? 0 : 1;
+$style         = isset( $instance['style'] ) ? $instance['style'] : 'portfolio-with-text';
+$column        = isset( $instance['column'] ) ? $instance['column'] : '4';
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+?>
+<?php
+
+if ( $filter && ! $categories ) {
+	$terms = get_terms( 'portfolio_cat' );
+	$count = count( $terms );
+	if ( $count > 0 ) {
+		foreach ( $terms as $term ) {
+			$output .= "<li><a data-filter='." . $term->slug . "'>" . $term->name . "</a></li>\n";
+		}
+	}
+}
+
+if ( '0' === $categories ) {
+	$terms          = get_terms( 'portfolio_cat' );
+	$included_terms = wp_list_pluck( $terms, 'term_id' );
+} else {
+	$included_terms = $categories;
+}
+
+$project_query = new WP_Query(
+	array(
+		'post_type'      => 'portfolio',
+		'posts_per_page' => $number,
+		'paged'=> $paged,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'portfolio_cat',
+				'field'    => 'id',
+				'terms'    => $included_terms,
+			),
+		),
+	)
+);?>
+
+
+<div class="portfolio-container">
+	<ul class="portfolio-items row  <?php echo $style ?> ">
+		<?php while ( $project_query->have_posts() ) : $project_query->the_post(); ?>
+			<?php
+			global $post;
+			$id          = $post->ID;
+			$image_per = get_the_post_thumbnail_url(null, $thumbnail_size );
+			$terms_array  = get_the_terms( $id, 'portfolio_cat' );
+			$term_string = '';
+
+			if ( $terms_array ) {
+				foreach ( $terms_array as $term ) {
+					$term_string .= $term->slug . ' ';
+				}
+			}
+			?>
+			<li class="portfolio-item <?php echo suffice_get_column_class( $column )?> <?php echo $term_string?>" data-category="<?php echo $term_string?>">
+				<figure class="portfolio-item-thumbnail">
+					<a href="#" ><div class="portfolio-item-img" style="background-image:url('<?php echo get_the_post_thumbnail_url($id, $thumbnail_size ) ?>')"></div></a>
+					<figcaption class="portfolio-item-description">
+						<p class="portfolio-item-price"><?php echo get_post_meta( $id, '_product_price', true )?></p>
+						<h5 class="portfolio-item-title"><a href = "<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h5>
+						<div class="portfolio-item-more"><a href = "<?php echo esc_url( get_the_permalink() ); ?>">READ MORE<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></div>
+					</figcaption>
+				</figure>
+			</li>
+		<?php endwhile; wp_reset_postdata();?>
+	</ul>
+	<?php
+	pagination_portfolio(array(
+		'post_type'             => 'portfolio',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'portfolio_cat',
+				'field'    => 'id',
+				'terms'    => $included_terms,
+			),
+		),
+	), $number);
+	?>
+</div>
